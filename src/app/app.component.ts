505b46da7338fcb30212e0805f19d1c8145155a2
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-20 19:33:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<app-register-page></app-register-page>`
 })
 export class AppComponent {
   title = 'SecondUseCase';
