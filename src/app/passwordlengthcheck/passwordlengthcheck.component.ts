3949e49b4e64c9394df37f47836b82e74d452c42
Gogulnath V <gonath222_code@users.noreply.github.com>
2021-04-22 13:45:55 +0530

    Update passwordlengthcheck.component.ts

--- src/app/passwordlengthcheck/passwordlengthcheck.component.ts
+++ src/app/passwordlengthcheck/passwordlengthcheck.component.ts
@@ -16,14 +16,14 @@ export class PasswordlengthcheckComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
+  ngOnChanges(changes: { [passwordText: string]: SimpleChange }): void {
     const password = changes.passwordToCheck.currentValue;
     if (password) {
       this.IsPasswordLengthCorrect = (password.length >= 1 && password.length <= 5) ? true : false;
     } else {
       this.IsPasswordLengthCorrect = false;
     }
-    this.IsPasswordLengthCorrect ? this.isValidPassword.emit(true) : this.isValidPassword.emit(false);
+    this.isValidPassword.emit(this.IsPasswordLengthCorrect);
   }
 
 }
